f371f01adb53ec223c22a4b0387121c8bb52c19f
takusankai <j021328@gmail.com>
2024-01-15 19:53:12 +0900

    指摘を受け、エラーの出るコマンドを使う形に修正

--- shell/stderr.sh
+++ shell/stderr.sh
@@ -1,5 +1,7 @@
-# 標準エラー出力 Hello, error! を error.txt にリダイレクトする
-echo "Hello, error!" >&2 > error.txt
+# 標準エラー出力を error.txt にリダイレクトする
+# （表示）cat: non_existent_file: No such file or directory
+cat non_existent_file 2> error.txt
 
 # >> で追記をする
-echo "more error" >&2 >> error.txt
+# （表示）ls: cannot access 'exist1': No such file or directory
+ls exist1 2>> error.txt
