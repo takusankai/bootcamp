c473c27b814902a4431ad4a832d2a8679839ecb5
takusankai <j021328@gmail.com>
2024-01-15 20:11:52 +0900

    引数の命名を変更

--- shell/stderr.sh
+++ shell/stderr.sh
@@ -1,7 +1,7 @@
 # 標準エラー出力を error.txt にリダイレクトする
-# （表示）cat: non_existent_file: No such file or directory
-cat non_existent_file 2> error.txt
+# （表示）cat: non-existent-file: No such file or directory
+cat non-existent-file 2> error.txt
 
 # >> で追記をする
-# （表示）ls: cannot access 'exist1': No such file or directory
-ls exist1 2>> error.txt
+# （表示）ls: cannot access 'non-existent': No such file or directory
+ls non-existent 2>> error.txt
