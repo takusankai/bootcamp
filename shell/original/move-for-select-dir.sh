1cd54d4c43e52ba028ed236b3a186b4155843741
takusankai <j021328@gmail.com>
2024-01-18 21:23:17 +0900

    単純な修正部分を先にコミット、詳細は説明に
    ・定数の変数名を大文字化
    ・referenceを複数形に変更
    ・#後のスペースを正しく追加
    ・write内の変数をmoveと統一
    ・phrase→patternに変更

--- shell/original/move-for-select-dir.sh
+++ shell/original/move-for-select-dir.sh
@@ -1,37 +1,37 @@
 # reference.txtは「探すファイル名に含まれる文字列,移動先フォルダ名」という記述形式が保証されているとする
-reference_path="./reference.txt"
+REFERENCE_PATH="./reference.txt"
 
 if [[ $1 == "write" ]]
 then
 
-  #writeが引数の場合、reference.txtへの書き込みを行う
+  # writeが引数の場合、references.txtへの書き込みを行う
 
-  read -p "移動したいファイルに含まれる文字列: " first_value
-  read -p "そのファイルの移動先フォルダ名: " second_value
+  read -p "移動したいファイルに含まれる文字列: " search_pattern
+  read -p "そのファイルの移動先フォルダ名: " move_directory
 
   # 「1つ目,2つ目」の形でreference.txtに出力
-  echo "$first_value,$second_value" >> $reference_path
+  echo "$search_pattern,$move_directory" >> $REFERENCE_PATH
 
-  echo "reference.txtに書き込みました"
+  echo "references.txtに書き込みました"
 
 elif [[ $1 == "move" ]]
 then
 
-  #通常時、reference.txtの内容を元にファイルを移動する
+  # 通常時、reference.txtの内容を元にファイルを移動する
 
-  # reference.txtの内容を行区切りで配列に保存
-  mapfile -t input_lines < $reference_path
+  # references.txtの内容を行区切りで配列に保存
+  mapfile -t input_lines < $REFERENCE_PATH
 
   for line in ${input_lines[@]}
   do
 
     # ','で「探す文字列」と「移動先名」に分割
     IFS=',' read -a parts <<< "$line"
-    search_phrase=${parts[0]}
+    search_pattern=${parts[0]}
     move_directory=${parts[1]}
 
-    # カレントディレクトリからsearch_phraseをファイル名に含むファイルを探し、配列に保存
-    mapfile -t move_file < <(find . -type f -name "*$search_phrase*")
+    # カレントディレクトリからsearch_patternをファイル名に含むファイルを探し、配列に保存
+    mapfile -t move_file < <(find . -type f -name "*$search_pattern*")
     
     # 見つけたファイルをmove_directoryに移動し、移動結果を表示
     for file in "${move_file[@]}"
@@ -46,7 +46,7 @@ then
 
 else
 
-  #引数が指定されていない場合、動作説明を表示
+  # 引数が指定されていない場合、動作説明を表示
   echo
   echo "--------------------------------------------------------------------------------------"
   echo -e "\033[31m第一引数に「write」か「move」を指定してください\033[m"
@@ -55,18 +55,18 @@ else
   echo "これは多数の講義ファイルを各講義フォルダに振り分け、整理することを想定して作られたスクリプトです。"
   echo "予め「"CV"と名の付くファイルは"コンピュータビジョン"フォルダに入れる」などと登録し、一括で整理することができます"
   echo "--------------------------------------------------------------------------------------"
-  echo "このスクリプトは、カレントディレクトリ内に登録の為のreference.txtを必要とします"
-  echo "reference.txtには「移動したいファイルに含まれる文字列」と「そのファイルの移動先フォルダ名」がセットで登録されます"
+  echo "このスクリプトは、カレントディレクトリ内に登録の為のreferences.txtを必要とします"
+  echo "references.txtには「移動したいファイルに含まれる文字列」と「そのファイルの移動先フォルダ名」がセットで登録されます"
   echo "これを参照してカレントディレクトリ内のファイルを移動し、整理します"
   echo "--------------------------------------------------------------------------------------"
   echo -e "\033[31m第一引数に「write」を指定 : \033[m"
-  echo "reference.txtに書き込みを行います"
+  echo "references.txtに書き込みを行います"
   echo "入力プロンプトに従って、移動したいファイルに含まれる文字列と、そのファイルの移動先フォルダ名を入力してください"
-  echo "書き込みが完了したら、「reference.txtに書き込みました」と表示します"
+  echo "書き込みが完了したら、「references.txtに書き込みました」と表示します"
   echo "--------------------------------------------------------------------------------------"
   echo -e "\033[31m第一引数に「move」を指定 : \033[m"
-  echo "reference.txtの内容を元にファイルを移動を行います"
-  echo "reference.txtの「検索する文字列」と「移動先フォルダ名」を参照し、これに従ってファイルを移動します"
+  echo "references.txtの内容を元にファイルを移動を行います"
+  echo "references.txtの「検索する文字列」と「移動先フォルダ名」を参照し、これに従ってファイルを移動します"
   echo "移動が完了したら、移動したファイル名と移動先フォルダ名の一覧を表示します"
   echo "--------------------------------------------------------------------------------------"
   echo
