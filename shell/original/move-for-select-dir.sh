5f2913a77bd2f21b6ce06f61e933d9737cf72932
takusankai <j021328@gmail.com>
2024-01-18 22:54:34 +0900

    修正の完了
    ・reference "s" .txt への変更漏れを修正
    ・「move_file→file_to_move」「move_directory→destination_directory」の修正
    ・動作説明を簡易化
    ・不要に配列を使っていた部分を cat コマンドのオプションを利用する形に変更

--- shell/original/move-for-select-dir.sh
+++ shell/original/move-for-select-dir.sh
@@ -1,5 +1,5 @@
-# reference.txtは「探すファイル名に含まれる文字列,移動先フォルダ名」という記述形式が保証されているとする
-REFERENCE_PATH="./reference.txt"
+# references.txtは「探すファイル名に含まれる文字列,移動先フォルダ名」という記述形式が保証されているとする
+REFERENCE_PATH="./references.txt"
 
 if [[ $1 == "write" ]]
 then
@@ -7,17 +7,17 @@ then
   # writeが引数の場合、references.txtへの書き込みを行う
 
   read -p "移動したいファイルに含まれる文字列: " search_pattern
-  read -p "そのファイルの移動先フォルダ名: " move_directory
+  read -p "そのファイルの移動先フォルダ名: " destination_directory
 
-  # 「1つ目,2つ目」の形でreference.txtに出力
-  echo "$search_pattern,$move_directory" >> $REFERENCE_PATH
+  # 「1つ目,2つ目」の形でreferences.txtに出力
+  echo "$search_pattern,$destination_directory" >> $REFERENCE_PATH
 
   echo "references.txtに書き込みました"
 
 elif [[ $1 == "move" ]]
 then
 
-  # 通常時、reference.txtの内容を元にファイルを移動する
+  # 通常時、references.txtの内容を元にファイルを移動する
 
   # references.txtの内容を行区切りで配列に保存
   mapfile -t input_lines < $REFERENCE_PATH
@@ -26,19 +26,18 @@ then
   do
 
     # ','で「探す文字列」と「移動先名」に分割
-    IFS=',' read -a parts <<< "$line"
-    search_pattern=${parts[0]}
-    move_directory=${parts[1]}
+    search_pattern=$(echo "$line" | cut -d',' -f1)
+    destination_directory=$(echo "$line" | cut -d',' -f2)
 
     # カレントディレクトリからsearch_patternをファイル名に含むファイルを探し、配列に保存
-    mapfile -t move_file < <(find . -type f -name "*$search_pattern*")
+    mapfile -t file_to_move < <(find . -type f -name "*$search_pattern*")
     
-    # 見つけたファイルをmove_directoryに移動し、移動結果を表示
-    for file in "${move_file[@]}"
+    # 見つけたファイルをdestination_directoryに移動し、移動結果を表示
+    for file in "${file_to_move[@]}"
     do
 
-      mv "$file" "./$move_directory/"
-      echo "$file >> $move_directory"
+      mv "$file" "./$destination_directory/"
+      echo "$file >> $destination_directory"
       
     done
 
@@ -47,28 +46,11 @@ then
 else
 
   # 引数が指定されていない場合、動作説明を表示
+  echo "第一引数に「write」か「move」を指定してください"
+  echo "まず、references.txtには「移動したいファイルに含まれる文字列」と「そのファイルの移動先フォルダ名」がセットで登録します"
+  echo "そして、これを参照してカレントディレクトリ内のファイルを移動し整理します"
   echo
-  echo "--------------------------------------------------------------------------------------"
-  echo -e "\033[31m第一引数に「write」か「move」を指定してください\033[m"
-  echo "--------------------------------------------------------------------------------------"
-  echo -e "\033[31m第一引数が正しく指定されていないので、動作説明を表示します\033[m"
-  echo "これは多数の講義ファイルを各講義フォルダに振り分け、整理することを想定して作られたスクリプトです。"
-  echo "予め「"CV"と名の付くファイルは"コンピュータビジョン"フォルダに入れる」などと登録し、一括で整理することができます"
-  echo "--------------------------------------------------------------------------------------"
-  echo "このスクリプトは、カレントディレクトリ内に登録の為のreferences.txtを必要とします"
-  echo "references.txtには「移動したいファイルに含まれる文字列」と「そのファイルの移動先フォルダ名」がセットで登録されます"
-  echo "これを参照してカレントディレクトリ内のファイルを移動し、整理します"
-  echo "--------------------------------------------------------------------------------------"
-  echo -e "\033[31m第一引数に「write」を指定 : \033[m"
-  echo "references.txtに書き込みを行います"
-  echo "入力プロンプトに従って、移動したいファイルに含まれる文字列と、そのファイルの移動先フォルダ名を入力してください"
-  echo "書き込みが完了したら、「references.txtに書き込みました」と表示します"
-  echo "--------------------------------------------------------------------------------------"
-  echo -e "\033[31m第一引数に「move」を指定 : \033[m"
-  echo "references.txtの内容を元にファイルを移動を行います"
-  echo "references.txtの「検索する文字列」と「移動先フォルダ名」を参照し、これに従ってファイルを移動します"
-  echo "移動が完了したら、移動したファイル名と移動先フォルダ名の一覧を表示します"
-  echo "--------------------------------------------------------------------------------------"
-  echo
+  echo "第一引数に「write」を指定 : 入力プロンプトに従って、references.txtに書き込みを行います"
+  echo "第一引数に「move」を指定 : references.txtの「検索する文字列」と「移動先フォルダ名」を参照し、これに従ってファイルを移動します"
 
 fi
