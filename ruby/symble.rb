a989db83cd67f22ab3ba84416308abf761f119a8
takusankai <j021328@gmail.com>
2024-01-19 23:56:35 +0900

    #ruby-7 「Symbolクラス について調べ、その利用例をコードする」 のスクリプトを作成

---
+++ ruby/symble.rb
@@ -0,0 +1,22 @@
+# Rubyの内部実装では、メソッド名や変数名`名前'を整数で管理して検索や比較で活用している
+# その整数をRubyのコード上で表現したものがシンボルである
+# string は mutable で、symbol は immutable である
+
+# string と symbol の比較
+str1 = "hoge"
+str2 = "hoge"
+puts str1.equal?(str2) #=> false
+puts str1.object_id    #=> 60
+puts str2.object_id    #=> 80
+
+sym1 = :hoge
+sym2 = :hoge
+puts sym1.equal?(sym2) #=> true
+puts sym1.object_id    #=> 1013148
+puts sym2.object_id    #=> 1013148
+
+puts str1.class #=> String
+puts sym1.class #=> Symbol
+
+puts 'fuga' == :fuga.to_s # シンボルを文字列に変換
+puts 'fuga'.to_sym == :fuga # 文字列をシンボルに変換
