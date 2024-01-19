12c7a6dd6154050ea0e607def791119c503962ff
takusankai <j021328@gmail.com>
2024-01-19 23:52:39 +0900

    #ruby-3 「Numericクラス（Integer / Float / Rational / Complex）について調べ、その利用例をコードする」 のスクリプトを作成

---
+++ ruby/numeric.rb
@@ -0,0 +1,23 @@
+# 整数の例
+int1 = 10
+int2 = 20
+int3 = int1 + int2
+puts "#{int3}"
+
+# 浮動小数点数の例
+flo1 = 3.14
+flo2 = 1.414
+flo3 = flo1 - flo2
+puts "#{flo3}"
+
+# 有理数の例
+rat1 = Rational(2, 3)
+rat2 = Rational(1, 4)
+rat3 = rat1 * rat2
+puts "#{rat3}"
+
+# 複素数の例
+com1 = Complex(1, 2)
+com2 = Complex(3, 4)
+com3 = com1 / com2
+puts "#{com3}"
