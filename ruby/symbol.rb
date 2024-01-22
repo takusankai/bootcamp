893f54cba23ebcf973178ca41d2fd9c2fae78aa1
takusankai <j021328@gmail.com>
2024-01-22 23:04:10 +0900

    省略形の解消

--- ruby/symbol.rb
+++ ruby/symbol.rb
@@ -3,20 +3,20 @@
 # string は mutable で、symbol は immutable である
 
 # string と symbol の比較
-str1 = "hoge"
-str2 = "hoge"
-puts str1.equal?(str2) #=> false
-puts str1.object_id    #=> 60
-puts str2.object_id    #=> 80
+string1 = "hoge"
+string2 = "hoge"
+puts string1.equal?(string2) #=> false
+puts string1.object_id    #=> 60
+puts string2.object_id    #=> 80
 
-sym1 = :hoge
-sym2 = :hoge
-puts sym1.equal?(sym2) #=> true
-puts sym1.object_id    #=> 1013148
-puts sym2.object_id    #=> 1013148
+symbol1 = :hoge
+symbol2 = :hoge
+puts symbol1.equal?(symbol2) #=> true
+puts symbol1.object_id    #=> 1013148
+puts symbol2.object_id    #=> 1013148
 
-puts str1.class #=> String
-puts sym1.class #=> Symbol
+puts string1.class #=> String
+puts symbol1.class #=> Symbol
 
 puts 'fuga' == :fuga.to_s # シンボルを文字列に変換
 puts 'fuga'.to_sym == :fuga # 文字列をシンボルに変換
