aeb9a88992d1ef3e794211abf7c3c03469ac2633
takusankai <j021328@gmail.com>
2024-01-22 20:49:50 +0900

    スタイルガイドの記述に気づき、アンダーバーで数字を区切らないように変更

--- ruby/numeric.rb
+++ ruby/numeric.rb
@@ -1,23 +1,23 @@
 # 整数の例
-integer_1 = 10
-integer_2 = 20
-integer_3 = integer_1 + integer_2
-puts "#{integer_3}"
+integer1 = 10
+integer2 = 20
+integer3 = integer1 + integer2
+puts "#{integer3}"
 
 # 浮動小数点数の例
-float_1 = 3.14
-float_2 = 1.414
-float_3 = float_1 - float_2
-puts "#{float_3}"
+float1 = 3.14
+float2 = 1.414
+float3 = float1 - float2
+puts "#{float3}"
 
 # 有理数の例
-rational_1 = Rational(2, 3)
-rational_2 = Rational(1, 4)
-rational_3 = rational_1 * rational_2
-puts "#{rational_3}"
+rational1 = Rational(2, 3)
+rational2 = Rational(1, 4)
+rational3 = rational1 * rational2
+puts "#{rational3}"
 
 # 複素数の例
-complex_1 = Complex(1, 2)
-complex_2 = Complex(3, 4)
-complex_3 = complex_1 / complex_2
-puts "#{complex_3}"
+complex1 = Complex(1, 2)
+complex2 = Complex(3, 4)
+complex3 = complex1 / complex2
+puts "#{complex3}"
