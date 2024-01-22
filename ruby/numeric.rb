6758986602ba546163fd42a95efbc085392ffbbb
takusankai <j021328@gmail.com>
2024-01-22 17:41:00 +0900

    変数名の省略を解消

--- ruby/numeric.rb
+++ ruby/numeric.rb
@@ -1,23 +1,23 @@
 # 整数の例
-int1 = 10
-int2 = 20
-int3 = int1 + int2
-puts "#{int3}"
+integer_1 = 10
+integer_2 = 20
+integer_3 = integer_1 + integer_2
+puts "#{integer_3}"
 
 # 浮動小数点数の例
-flo1 = 3.14
-flo2 = 1.414
-flo3 = flo1 - flo2
-puts "#{flo3}"
+float_1 = 3.14
+float_2 = 1.414
+float_3 = float_1 - float_2
+puts "#{float_3}"
 
 # 有理数の例
-rat1 = Rational(2, 3)
-rat2 = Rational(1, 4)
-rat3 = rat1 * rat2
-puts "#{rat3}"
+rational_1 = Rational(2, 3)
+rational_2 = Rational(1, 4)
+rational_3 = rational_1 * rational_2
+puts "#{rational_3}"
 
 # 複素数の例
-com1 = Complex(1, 2)
-com2 = Complex(3, 4)
-com3 = com1 / com2
-puts "#{com3}"
+complex_1 = Complex(1, 2)
+complex_2 = Complex(3, 4)
+complex_3 = complex_1 / complex_2
+puts "#{complex_3}"
