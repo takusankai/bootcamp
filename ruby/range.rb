849a09594cb33d63cdae1381576faf1b5fc64f84
takusankai <j021328@gmail.com>
2024-01-23 20:06:31 +0900

    出力結果の表現方法の統一と()周りの不要なスペースの削除

--- ruby/range.rb
+++ ruby/range.rb
@@ -10,10 +10,10 @@ puts "Sum: #{sum}"
 
 # 文字列の範囲指定と含まれているかの判定
 string_range = 'a'..'f'
-p string_range.include? 'e' # true
-p string_range.include? 'z' # false
+p string_range.include? 'e' # => true
+p string_range.include? 'z' # => false
 
 # 日付の範囲指定と含まれているかの判定
 require 'date'
-p ( Date.parse("2024-01-01")..Date.parse("2024-01-31") ).include? Date.parse("2024-01-19") # true
-p ( Date.parse("2024-01-01")..Date.parse("2024-01-31") ).include? Date.parse("2024-02-01") # false
+p (Date.parse("2024-01-01")..Date.parse("2024-01-31")).include? Date.parse("2024-01-19") # => true
+p (Date.parse("2024-01-01")..Date.parse("2024-01-31")).include? Date.parse("2024-02-01") # => false
