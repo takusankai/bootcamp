ba1aa57e5c5b0c161b0fe115c451a443f8068ad3
takusankai <j021328@gmail.com>
2024-01-23 18:44:08 +0900

    コメントアウトの開始位置やスペースの統一

--- ruby/symbol.rb
+++ ruby/symbol.rb
@@ -5,18 +5,20 @@
 # string と symbol の比較
 string1 = "hoge"
 string2 = "hoge"
-puts string1.equal?(string2) #=> false
-puts string1.object_id    #=> 60
-puts string2.object_id    #=> 80
+puts string1.equal?(string2) # => false
+puts string1.object_id       # => 60
+puts string2.object_id       # => 80
 
 symbol1 = :hoge
 symbol2 = :hoge
-puts symbol1.equal?(symbol2) #=> true
-puts symbol1.object_id    #=> 1013148
-puts symbol2.object_id    #=> 1013148
+puts symbol1.equal?(symbol2) # => true
+puts symbol1.object_id       # => 1013148
+puts symbol2.object_id       # => 1013148
 
-puts string1.class #=> String
-puts symbol1.class #=> Symbol
+puts string1.class           # => String
+puts symbol1.class           # => Symbol
 
-puts 'fuga' == :fuga.to_s # シンボルを文字列に変換
-puts 'fuga'.to_sym == :fuga # 文字列をシンボルに変換
+# シンボルを文字列に変換
+puts 'fuga' == :fuga.to_s
+# 文字列をシンボルに変換
+puts 'fuga'.to_sym == :fuga
