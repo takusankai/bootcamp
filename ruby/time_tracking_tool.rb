40013d7c9a71bf9ab50157cbea3f1a35eae7624b
takusankai <j021328@gmail.com>
2024-01-29 17:27:52 +0900

    4点の修正、詳細を開いてください
    ファイル読み書き部分での名前変更、記録するファイル名を「task_log.csv」に変更
    start, finish メソッドで Time.now を呼ぶ場所を記録する直前に変更し余計な変数を省略
    @tasks.each を @tasks.reduce に変更し、宣言し再代入する冗長さを排除
    culclate_task_time メソッドで余計な記述を1行にまとめる修正

--- ruby/time_tracking_tool.rb
+++ ruby/time_tracking_tool.rb
@@ -2,17 +2,16 @@ require 'date'
 require 'time'
 require 'csv'
 
-module FileIO
-  REFERENCE='references.csv'
-
+module TaskLogAccessor
+  TASK_LOG='task_log.csv'
 
   def load_file
-    return [] if File.empty?(REFERENCE)
-    CSV.read(REFERENCE, headers: true)
+    return [] if File.empty?(TASK_LOG)
+    CSV.read(TASK_LOG, headers: true)
   end
 
   def record_file(data_list)
-    CSV.open(REFERENCE, 'w') do |csv|
+    CSV.open(TASK_LOG, 'w') do |csv|
       csv << ["name", "start_date_time", "end_date_time"]
       data_list.each { |row| csv << row }
     end
@@ -20,31 +19,29 @@ module FileIO
 end
 
 class TaskRecorder
-  include FileIO
+  include TaskLogAccessor
 
-  def initialize(name: nil, start_date_time: nil, end_date_time: nil)
+  def initialize(name: nil)
     @name = name
-    @start_date_time = start_date_time
-    @end_date_time = end_date_time
   end
 
   def start
     tasks = load_file
     return puts "タスク「#{@name}」はすでに存在しています。" if tasks.any? { |task| task['name'] == @name }
-    tasks << [@name, @start_date_time, @end_date_time]
+    tasks << [@name, Time.now, nil]
     record_file(tasks)
   end
 
   def finish
     tasks = load_file
     return puts "タスク「#{@name}」は存在しません。" if tasks.none? { |task| task['name'] == @name }
-    tasks.each { |task| task['end_date_time'] = @end_date_time if task['name'] == @name }
+    tasks.each { |task| task['end_date_time'] = Time.now if task['name'] == @name }
     record_file(tasks)
   end
 end
 
 class TaskList
-  include FileIO
+  include TaskLogAccessor
 
   def initialize
     @tasks = load_file
@@ -67,18 +64,17 @@ class TaskList
 
   def show_weekly
     puts "以下が、今週の日付ごと及び累計の作業時間です"
-    sum_weekly_time = 0
-    time_each_day = Array.new(7, 0)
-    @tasks.each do |task|
+    sum_weekly_time, time_each_day = @tasks.reduce([0, Array.new(7, 0)]) do |(sum, seven_days), task|
       if Date.parse(task['start_date_time']) > Date.today - 7
         task_time = culclate_task_time(task)
-        sum_weekly_time += task_time
-        time_each_day[(Date.parse(task['start_date_time']) - Date.today).abs.to_i] += task_time
+        sum += task_time
+        seven_days[(Date.parse(task['start_date_time']) - Date.today).abs.to_i] += task_time
       end
+      [sum, seven_days]
     end
-    time_each_day.each_with_index do |time, index|
+    time_each_day.each_with_index do |daily_time, index|
       print("#{index}日前の作業時間: ")
-      print_hms_style(time)
+      print_hms_style(daily_time)
     end
     print("今週の累計作業時間: ")
     print_hms_style(sum_weekly_time)
@@ -88,10 +84,7 @@ class TaskList
     if task['end_date_time'].nil?
       task_working_time = Time.now - Time.parse(task['start_date_time'])
     end
-    start_time = Time.parse(task['start_date_time'])
-    end_time = Time.parse(task['end_date_time'])
-    task_working_time = end_time - start_time
-    task_working_time
+    task_working_time = Time.parse(task['end_date_time']) - Time.parse(task['start_date_time'])
   end
 
   def print_hms_style(second_style_time)
@@ -129,7 +122,7 @@ class ArgumentManager
       display_usage
       return
     end
-    task = TaskRecorder.new(name: @task_name, start_date_time: Time.now)
+    task = TaskRecorder.new(name: @task_name)
     task.start
   end
 
@@ -138,7 +131,7 @@ class ArgumentManager
       display_usage
       return
     end
-    task = TaskRecorder.new(name: @task_name, end_date_time: Time.now)
+    task = TaskRecorder.new(name: @task_name)
     task.finish
   end
 
