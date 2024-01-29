6e37a2086662f940b14598511c855412b0f577f6
takusankai <j021328@gmail.com>
2024-01-29 20:43:15 +0900

    追加機能として、乾燥機能を作成

--- ruby/washing-machine.rb
+++ ruby/washing-machine.rb
@@ -24,14 +24,28 @@ class WashingMachine
   def wash_clothes
     @clothes_array.each do |clothes|
       clothes.cleanliness = true
+      clothes.dryness = false
       puts "#{clothes.name}が清潔になりました。"
     end
+  end
+
+  def dry_clothes
+    @clothes_array.each do |clothes|
+      clothes.dryness = true
+      puts "#{clothes.name}が乾燥機能で乾かされました。"
+    end
+  end
+
+  def wash_and_dry
+    wash_clothes
+    dry_clothes
   end
 
   def output_array_status
     puts "以下が洗濯機の中の衣類の一覧です。"
     @clothes_array.each_with_index do |clothes, index|
-      puts "#{index} : #{clothes.name} : #{clothes.cleanliness ? '清潔' : '不清潔'}"
+      print "#{index} : #{clothes.name} : #{clothes.cleanliness ? '清潔' : '不清潔'} : "
+      puts "#{clothes.dryness ? '乾いている' : '乾いていない'}"
     end
     puts "以上の#{@clothes_array.length}点が洗濯機の中の衣類になります。"
   end
@@ -39,19 +53,21 @@ end
 
 class Clothes
   attr_reader :name
-  attr_accessor :cleanliness
+  attr_accessor :cleanliness, :dryness
 
-  def initialize(name, cleanliness = true)
+  def initialize(name, cleanliness = true, dryness = true)
     @name = name
     @cleanliness = cleanliness
+    @dryness = dryness
   end
 
   def use
-    if @cleanliness
+    if @cleanliness && @dryness
       @cleanliness = false
       puts "#{@name}を使いました。"
     else
-      puts "#{@name}は洗濯しないと使えません。"
+      puts "#{@name}は洗濯しないと使えません。" unless @cleanliness
+      puts "#{@name}は乾燥しないと使えません。" unless @dryness
     end
   end
 end
@@ -75,7 +91,7 @@ washing_machine.add_clothes([t_shirt_b, t_shirt_c])
 
 washing_machine.output_array_status
 
-washing_machine.wash_clothes
+washing_machine.wash_and_dry
 
 washing_machine.remove_clothes([t_shirt_a])
 washing_machine.remove_clothes([t_shirt_b, t_shirt_c])
@@ -83,3 +99,13 @@ washing_machine.remove_clothes([t_shirt_b, t_shirt_c])
 puts "TシャツAは清潔ですか？: #{t_shirt_a.cleanliness ? '清潔' : '不清潔'}"
 puts "TシャツBは清潔ですか？: #{t_shirt_b.cleanliness ? '清潔' : '不清潔'}"
 puts "TシャツCは清潔ですか？: #{t_shirt_c.cleanliness ? '清潔' : '不清潔'}"
+
+puts "以下は、追加の乾燥機能に関する一連の確認です。"
+t_shirt_d = Clothes.new('TシャツD')
+t_shirt_d.use
+washing_machine.add_clothes([t_shirt_d])
+washing_machine.wash_clothes
+washing_machine.output_array_status
+washing_machine.dry_clothes
+washing_machine.remove_clothes([t_shirt_d])
+puts "TシャツAは乾いていますか？: #{t_shirt_d.dryness ? '乾いている' : '乾いていない'}"
