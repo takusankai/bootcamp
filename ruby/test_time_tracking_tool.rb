398d6cdc72a1f5a71b0a6ff7521333c18f13c5c8
takusankai <j021328@gmail.com>
2024-01-31 19:00:07 +0900

    例外の動作に関する確認は、適当なオプションを受け取った時だけするif文を追加

--- ruby/test_time_tracking_tool.rb
+++ ruby/test_time_tracking_tool.rb
@@ -27,24 +27,26 @@ ArgumentManager.new(option: "-f", task_name: second_task).switch_option
 ArgumentManager.new(option: "-st", task_name: nil).switch_option          # 終了していないタスクがない状態で表示
 ArgumentManager.new(option: "-sw", task_name: nil).switch_option          # 終了していないタスクがない状態で表示
 
-# 想定される引数以外を渡した場合の挙動を確認
-# usageを表示する
-ArgumentManager.new(option: nil, task_name: nil).switch_option          # 引数がない
-p("↑引数無し" + "-" * 50)
-ArgumentManager.new(option: "-a", task_name: nil).switch_option         # 定義されていないオプション
-p("↑引数無し" + "-" * 50)
-ArgumentManager.new(option: "-s", task_name: nil).switch_option         # -s のタスク名がない
-p("↑-s名前なし" + "-" * 50)
-ArgumentManager.new(option: "-f", task_name: nil).switch_option         # -f のタスク名がない
-p("↑-f名前なし" + "-" * 50)
-ArgumentManager.new(option: "-st", task_name: first_task).switch_option # -st の引数が多い
-p("↑-st引数多" + "-" * 50)
-ArgumentManager.new(option: "-sw", task_name: first_task).switch_option # -sw の引数が多い
-p("↑-sw引数多" + "-" * 50)
+# 何らかのオプションを渡した場合のみ、例外の挙動を確認
+if ARGV[0] != nil
+  # usageを表示する
+  ArgumentManager.new(option: nil, task_name: nil).switch_option          # 引数がない
+  p("↑引数無し" + "-" * 50)
+  ArgumentManager.new(option: "-a", task_name: nil).switch_option         # 定義されていないオプション
+  p("↑引数無し" + "-" * 50)
+  ArgumentManager.new(option: "-s", task_name: nil).switch_option         # -s のタスク名がない
+  p("↑-s名前なし" + "-" * 50)
+  ArgumentManager.new(option: "-f", task_name: nil).switch_option         # -f のタスク名がない
+  p("↑-f名前なし" + "-" * 50)
+  ArgumentManager.new(option: "-st", task_name: first_task).switch_option # -st の引数が多い
+  p("↑-st引数多" + "-" * 50)
+  ArgumentManager.new(option: "-sw", task_name: first_task).switch_option # -sw の引数が多い
+  p("↑-sw引数多" + "-" * 50)
 
-# 例外のコメントを表示する
-ArgumentManager.new(option: "-s", task_name: second_task).switch_option # 開始するタスク名が被っている
-p("-" * 50)
-ArgumentManager.new(option: "-f", task_name: second_task).switch_option # 終了するタスク名が被っている
-p("-" * 50)
-ArgumentManager.new(option: "-f", task_name: third_task).switch_option  # 存在しないタスクを終了しようとしている
+  # 例外のコメントを表示する
+  ArgumentManager.new(option: "-s", task_name: second_task).switch_option # 開始するタスク名が被っている
+  p("-" * 50)
+  ArgumentManager.new(option: "-f", task_name: second_task).switch_option # 終了するタスク名が被っている
+  p("-" * 50)
+  ArgumentManager.new(option: "-f", task_name: third_task).switch_option  # 存在しないタスクを終了しようとしている
+end
