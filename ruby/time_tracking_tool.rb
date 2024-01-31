4a75a8a617ab3b6b6ead457db4cc66524c9af5b8
takusankai <j021328@gmail.com>
2024-01-31 18:24:01 +0900

    変数名を task_time → daily_task_time と修正

--- ruby/time_tracking_tool.rb
+++ ruby/time_tracking_tool.rb
@@ -67,12 +67,12 @@ class TaskList
   def show_week
     puts "以下が、今週の日付ごと及び累計の作業時間です"
     sum_week_time = ((Date.today - 6)..Date.today).sum do |date|
-      task_time = @tasks.sum do |task|
-        Time.parse(task['start_date_time']).to_date == date ? culclate_task_time(task) : 0
+      daily_task_time = @tasks.sum do |task|
+        Time.parse(task['start_date_time']).to_date == date ? culclate_daily_task_time(task) : 0
       end
       print("#{date.strftime('%m/%d')}の作業時間: ")
-      print_hms_style(task_time)
-      task_time
+      print_hms_style(daily_task_time)
+      daily_task_time
     end
     print("今週の累計作業時間: ")
     print_hms_style(sum_week_time)
